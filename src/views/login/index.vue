4521504c756b7cd9d1e927b165003b250f8eafe2
geeksuperbin <liubindashen@gmail.com>
2022-02-14 10:10:55 +0800

    登录页修改名称，增删改查后端功能测试工具

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">增删改查后端功能测试工具</h3>
       </div>
 
       <el-form-item prop="username">
