36ea21e06f1566e6e96366bbd9885115a7d1bf11
geeksuperbin <liubindashen@gmail.com>
2022-02-14 10:53:48 +0800

    src / utils / request.js 的 axios 封装研究

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,9 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: '增删改查前端展示工具',
+      // logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: ''
     }
   }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -3,23 +3,34 @@ import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
-// create an axios instance
+// create an axios instance  创建axios实例
+// 对 service 实例 进行操作，数据都封装在其中
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
 
-// request interceptor
+
+/*
+  第二步是添加拦截器， axios每次请求都会经过这个拦截器。 
+  store.getters.token 首先从全局状态管理那里判断 token ,
+  有的话就可以请求， 否则就抛出异常。并且可以清楚的看到如果有 token的话， 
+  我们在请求的时候会得到当前的token 并且直接添加到请求头中， 以便后端得到当前登录的信息。
+  
+  原文链接：https://blog.csdn.net/qq_41291945/article/details/107748231
+*/
+
+// request interceptor 请求拦截器
 service.interceptors.request.use(
   config => {
-    // do something before request is sent
+    // do something before request is sent 在发送请求之前做些什么
 
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['X-Token'] = getToken() 
     }
     return config
   },
@@ -31,6 +42,7 @@ service.interceptors.request.use(
 )
 
 // response interceptor
+// 对返回结果的拦截， 在这里会判断返回结果中的状态码， 根据状态码来判断这次请求的状态如何
 service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
